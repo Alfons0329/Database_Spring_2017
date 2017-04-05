a3cde7e06e609cdf42267233d36491f4622ec611
afhwu0329 <alfons.cs04@g2.nctu.edu.tw>
2017-04-06 01:15:27 +0800

    SQL Script till problem 8 (working on 9)

--- 20170328hw2/hw2script.sql
+++ 20170328hw2/hw2script.sql
@@ -34,16 +34,18 @@ from Master m,Salaries s
 where (m.playerID=s.playerID) and (s.salary=33000000); #same procedure
 
 #curse during 1920~2004
-#9-1 the highest paid debutfinalGameplayer during these days
+#9-1 the highest paid Gameplayer during these days
 select nameFirst,nameLast
 from Master m,Salaries s,Teams t,Pitching p,Batting b
-where (m.playerID=s.playerID)
+where 
+(m.playerID=s.playerID)
 and (m.playerID=b.playerID or m.playerID=p.playerID)
 and (b.teamID=t.teamID or p.teamID=t.teamID)
 and (t.LgWin='Y')
-and (m.debut>='1920-01-01') 
-and (m.finalGame<='2004-12-31') 
+and (m.debut between '1920-01-01' and '2004-12-31') 
+and (m.finalGame between '1920-01-01' and '2004-12-31')
 and (s.salary in (select max(salary)from Salaries)  #nested query
 );
 
+#9-2 the hightest paid Redsock player during these days
 
